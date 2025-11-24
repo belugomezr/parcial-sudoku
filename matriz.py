5bd2695a1f84772df5be61a1cb06e2dc57dde78e
Belen Gomez <115595107+belugomezr@users.noreply.github.com>
2025-11-24 18:05:07 -0300

    cambios

--- matriz.py
+++ matriz.py
@@ -1,13 +1,13 @@
 
 import random
 
-def inicializar_matriz(cantidad_filas: int, cantidad_columnas: int, valor:int) -> list:
+def inicializar_matriz(cantidad_filas: int, cantidad_columnas: int, valor:int) -> list: #Inicializo la matriz
     matriz = []
     for i in range(cantidad_filas):
         fila = []
         for _ in range(cantidad_columnas):
             fila.append(valor)
-        matriz.append(fila)
+        matriz.append(fila) #Añade la fila completa a la matriz.
     return matriz
 
 def chequear_fila(matriz:list, fila:int, numero:int):
@@ -36,9 +36,9 @@ def chequear_region(matriz:list, fila:int, columna:int, numero:int):
 
     return retorno 
 
-def validar_numero(matriz, fila, columna, numero):
+def validar_numero(matriz, fila, columna, numero): #validamos el numero 
     if numero == 0:
-        return True   # una celda vacía NO es un error
+        return True  
     valor_anterior = matriz[fila][columna]
     matriz[fila][columna] = 0
 
@@ -47,14 +47,14 @@ def validar_numero(matriz, fila, columna, numero):
     region_ok = not chequear_region(matriz, fila, columna, numero)
 
     matriz[fila][columna] = valor_anterior
-    return fila_ok and columna_ok and region_ok
+    return fila_ok and columna_ok and region_ok #retorna el numero si es que pasa todos los chequeos
 
-def encontrar_vacio(matriz):
+def encontrar_vacio(matriz): #busca la siguiente celda vacia para ingresar otro numero (donde)
     for i in range(9):
         for j in range(9):
             if matriz[i][j] == 0:
                 return i, j
-    return None
+    return None 
 
 def generar_tablero_completo():
     matriz = [[0]*9 for _ in range(9)]
@@ -70,22 +70,22 @@ def region_completa(matriz, fila, columna):
                 return False
     return True
 
-def resolver_sudoku_aleatorio(matriz):
-    pos = encontrar_vacio(matriz)
-    if pos is None:
+def resolver_sudoku_aleatorio(matriz): #Llena el tablero para lograr encontrar una solucion valida 
+    pos = encontrar_vacio(matriz) 
+    if pos is None: #sino hay celdas vacias retorna True
         return True
     fila, col = pos
     numeros = list(range(1, 10))
     random.shuffle(numeros)
-    for numero in numeros:
-        if validar_numero(matriz, fila, col, numero):
+    for numero in numeros: #recorre los numeros aletarios
+        if validar_numero(matriz, fila, col, numero): 
             matriz[fila][col] = numero
             if resolver_sudoku_aleatorio(matriz):
                 return True
             matriz[fila][col] = 0
-    return False
+    return False #esto permite probar otras combinaciones
 
-def resolver_sudoku_conteo(matriz, limite=2):
+def resolver_sudoku_conteo(matriz, limite=2): #Verificamos que tenga al menos dos soluciones 
     """
     Cuenta cuántas soluciones tiene el sudoku.
     Se detiene si encuentra más de 'limite'.
@@ -98,10 +98,9 @@ def resolver_sudoku_conteo(matriz, limite=2):
     soluciones = 0
 
     for num in range(1, 10):
-        if validar_numero(matriz, fila, col, num):
+        if validar_numero(matriz, fila, col, num): #Intentamos cada número posible en la celda vacía, validando fila, columna y región
             matriz[fila][col] = num
-            soluciones += resolver_sudoku_conteo(matriz, limite)
-
+            soluciones += resolver_sudoku_conteo(matriz, limite) 
             if soluciones >= limite:
                 break
 
@@ -115,7 +114,7 @@ def tablero_completo(matriz):
             return False
     return True
 
-def generar_tablero_facil_por_region(numeros_por_region):
+def generar_tablero_facil_por_region(numeros_por_region): #genera los numeros por region y dificultad
     matriz = inicializar_matriz(9, 9, 0)
 
     for region_fila in range(3):
@@ -140,7 +139,7 @@ def generar_tablero_facil_por_region(numeros_por_region):
     return matriz
 
 def crear_sudoku_con_pistas(tablero_completo, pistas_por_region=5):
-    sudoku = [fila[:] for fila in tablero_completo]
+    sudoku = [fila[:] for fila in tablero_completo] #hace una copia del tablero completo para no modificar el original
 
     # posiciones del tablero mezcladas
     posiciones = [(fila, col) for fila in range(9) for col in range(9)]
@@ -165,7 +164,7 @@ def crear_sudoku_con_pistas(tablero_completo, pistas_por_region=5):
         if pistas_totales <= pistas_por_region * 9:
             break
 
-    return sudoku
+    return sudoku #Devuelve el Sudoku jugable con las pistas finales
 
 def region_correcta(matriz, region_f, region_c):
     numeros = []
@@ -177,7 +176,7 @@ def region_correcta(matriz, region_f, region_c):
             numeros.append(num)
     return len(numeros) == 9 and len(set(numeros)) == 9  # sin repetidos
 
-def actualizar_puntaje(puntaje, matriz, fila, columna, numero, celda_incorrecta):
+def actualizar_puntaje(puntaje, matriz, fila, columna, numero, celda_incorrecta): 
     if validar_numero(matriz, fila, columna, numero):
         puntaje += 1
         if tablero_completo(matriz):
